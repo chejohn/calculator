d50d80e029fcb3a30daf5b084b84fd59b3d8e364
Ché John <chejohn16@gmail.com>
2021-05-23 23:01:52 -0400

    implemented all operations and functions on calculator. Fix overflow bug and add minor GUI tweaks

--- calculator.js
+++ calculator.js
@@ -1,21 +1,34 @@
 
 function add(num1, num2) {
-    return num1 + num2;
+    return String(num1 + num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return String(num1 - num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return String(num1 * num2);
 }
 
 function divide(num1, num2) {
-    return num1/num2;
+    if (num2 === 0) return 'Error';
+    return String(num1/num2);
+}
+
+function divideBy100(num1) {
+    return String(num1/100)
+}
+
+function updateNumbers() {
+    if (operationValue === null)
+        displayValues['num1'] = display.textContent;
+    else displayValues['num2'] = display.textContent;
 }
 
 function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     if (operator === '+') return add(num1, num2);
     else if (operator === '-') return subtract(num1, num2);
     else if (operator === '*') return multiply(num1, num2);
@@ -23,22 +36,35 @@ function operate(operator, num1, num2) {
 }
 
 function changeDisplay() {
-    if (display.textContent === '0' || (operationValue !== null && displayValues['num2'] === null)) {
+    if (this.textContent === '.' && (operationValue!== null && displayValues['num2'] === null)) {
+        display.textContent = '0' + this.textContent;
+    }
+    else if (this.textContent === '.') {
+        display.textContent = display.textContent + this.textContent;
+    } 
+    else if (display.textContent === '0' || (operationValue !== null && displayValues['num2'] === null)) {
         display.textContent = this.textContent; 
     }
     else display.textContent = display.textContent + this.textContent;
-    
-    if (operationValue === null)
-        displayValues['num1'] = display.textContent;
-    else displayValues['num2'] = display.textContent;
+    updateNumbers();
 }
 
 function storeOperation() {
+    if (displayValues['num2'] !== null) calculate();
     operationValue = this.textContent;
 }
 
 function calculate() {
-    
+    if (operationValue === null) return;
+    if (displayValues['num2'] === null) {
+        display.textContent = operate(operationValue, displayValues['num1'], displayValues['num1']);
+    }
+    else {
+        display.textContent = operate(operationValue, displayValues['num1'], displayValues['num2']);
+    }
+    displayValues['num1'] = display.textContent;
+    displayValues['num2'] = null;
+    operationValue = null;
 }
 
 function clearDisplay() {
@@ -46,6 +72,21 @@ function clearDisplay() {
     displayValues['num2'] = null;
     operationValue = null;
     display.textContent = '0';
+}
+
+function changeSign() {
+    if (display.textContent === '0') return;
+    if (display.textContent.includes('-')) {
+        display.textContent = display.textContent.replace('-', '');
+    }
+    else display.textContent = '-' + display.textContent;
+    updateNumbers();
+}
+
+function makeAPercent() {
+    if (display.textContent === '0') return;
+    display.textContent = divideBy100(Number(display.textContent));
+    updateNumbers();
 }
 
 const display = document.getElementById('sub-display');
@@ -56,7 +97,6 @@ const displayValues = {
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
-    if (number.textContent === '.') return;
     number.addEventListener('click', changeDisplay)
 });
 
@@ -72,3 +112,9 @@ clear.addEventListener('click', clearDisplay);
 
 const equals = document.querySelector('#equals')
 equals.addEventListener('click', calculate);
+
+const posNeg = document.querySelector('#pos-neg');
+posNeg.addEventListener('click', changeSign)
+
+const percent = document.querySelector('#percent');
+percent.addEventListener('click', makeAPercent)
