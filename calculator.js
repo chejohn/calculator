c6faf1ff55dd228dd67080cb7019ac6d574ba11d
Ché John <chejohn16@gmail.com>
2021-05-25 15:16:42 -0400

    all operations are fully implemented. Must make tweaks to GUI to prevent integer overload

--- calculator.js
+++ calculator.js
@@ -35,41 +35,36 @@ function operate(operator, num1, num2) {
     else return divide(num1, num2);
 }
 
-function changeDisplay() {
-    if (this.textContent === '.' && (operationValue!== null && displayValues['num2'] === null)) {
-        display.textContent = '0' + this.textContent;
-    }
-    else if (this.textContent === '.') {
-        display.textContent = display.textContent + this.textContent;
-    } 
-    else if (display.textContent === '0' || (operationValue !== null && displayValues['num2'] === null)) {
+function populateNumbers() {
+    if (display.textContent === '0' || (operationValue !== null && displayValues['num2'] === '')) {
         display.textContent = this.textContent; 
     }
     else display.textContent = display.textContent + this.textContent;
+    
     updateNumbers();
 }
 
 function storeOperation() {
-    if (displayValues['num2'] !== null) calculate();
+    if (displayValues['num2'] !== '') calculate();
     operationValue = this.textContent;
 }
 
 function calculate() {
     if (operationValue === null) return;
-    if (displayValues['num2'] === null) {
+    if (displayValues['num2'] === '') {
         display.textContent = operate(operationValue, displayValues['num1'], displayValues['num1']);
     }
     else {
         display.textContent = operate(operationValue, displayValues['num1'], displayValues['num2']);
     }
     displayValues['num1'] = display.textContent;
-    displayValues['num2'] = null;
+    displayValues['num2'] = '';
     operationValue = null;
 }
 
 function clearDisplay() {
-    displayValues['num1'] = null;
-    displayValues['num2'] = null;
+    displayValues['num1'] = '';
+    displayValues['num2'] = '';
     operationValue = null;
     display.textContent = '0';
 }
@@ -84,20 +79,44 @@ function changeSign() {
 }
 
 function makeAPercent() {
-    if (display.textContent === '0') return;
     display.textContent = divideBy100(Number(display.textContent));
+    updateNumbers();
+}
+
+function brightenColor() {
+    if (this.classList.contains('number')) this.style.backgroundColor = 'hsl(181, 58%, 50%)';
+    else if (this.classList.contains('function')) this.style.backgroundColor = 'hsl(210, 100%, 75%)';
+    else this.style.backgroundColor = 'hsl(30, 100%, 75%)';
+}
+
+function revertColor() {
+    if (this.classList.contains('number')) this.style.backgroundColor = 'hsl(181, 58%, 31%)'
+    else if (this.classList.contains('function')) this.style.backgroundColor = 'hsl(210, 100%, 56%)';
+    else this.style.backgroundColor = 'hsl(30, 100%, 56%)';
+}
+
+function insertDecimal() {
+    if (operationValue === null && displayValues['num1'].includes('.')) return;
+    if (operationValue !== null && displayValues['num2'].includes('.')) return;
+    if (operationValue !== null && displayValues['num2'] === '') {
+        display.textContent = '0' + this.textContent;
+    }
+    else display.textContent = display.textContent + this.textContent;
     updateNumbers();
 }
 
 const display = document.getElementById('sub-display');
 const displayValues = {
-    num1: null,
-    num2: null
+    num1: '',
+    num2: ''
 };
 
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
-    number.addEventListener('click', changeDisplay)
+    if (number.id === 'decimal') return;
+    number.addEventListener('click', populateNumbers);
+    number.addEventListener('mousedown', brightenColor);
+    number.addEventListener('mouseup', revertColor);
 });
 
 let operationValue = null;
@@ -105,16 +124,28 @@ const operations = document.querySelectorAll('.operation');
 operations.forEach((operation) => {
     if (operation.textContent === '=') return;
     operation.addEventListener('click', storeOperation);
+    operation.addEventListener('click', brightenColor);
+});
+
+const auxFunctions = document.querySelectorAll('.function');
+auxFunctions.forEach((auxFunction) => {
+    auxFunction.addEventListener('mousedown', brightenColor);
+    auxFunction.addEventListener('mouseup', revertColor);
 });
 
 const clear = document.getElementById('clear');
 clear.addEventListener('click', clearDisplay);
 
-const equals = document.querySelector('#equals')
+const equals = document.querySelector('#equals');
 equals.addEventListener('click', calculate);
+equals.addEventListener('mousedown', brightenColor);
+equals.addEventListener('mouseup', revertColor);
 
 const posNeg = document.querySelector('#pos-neg');
-posNeg.addEventListener('click', changeSign)
+posNeg.addEventListener('click', changeSign);
 
 const percent = document.querySelector('#percent');
-percent.addEventListener('click', makeAPercent)
+percent.addEventListener('click', makeAPercent);
+
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', insertDecimal);
